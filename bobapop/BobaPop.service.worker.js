147f97f2143976ab1ec2de86c28d6086c76250b2
Dominik Maier <dmnk@google.com>
2025-01-26 18:46:59 +0100

    Update Gamejam Game

--- bobapop/BobaPop.service.worker.js
+++ bobapop/BobaPop.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737908838|1378937289';
+const CACHE_VERSION = '1737913577|114210531';
 /** @type {string} */
 const CACHE_PREFIX = 'BobaPop-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
