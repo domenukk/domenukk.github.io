c42c4137c47b8a11a11b579ffaab4d58b9d96c3d
Dominik Maier <dmnk@google.com>
2025-01-26 23:50:52 +0100

    BobaPop, best version yet

--- bobapop/BobaPop.service.worker.js
+++ bobapop/BobaPop.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737930259|3109871911';
+const CACHE_VERSION = '1737931751|4601789969';
 /** @type {string} */
 const CACHE_PREFIX = 'BobaPop-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
