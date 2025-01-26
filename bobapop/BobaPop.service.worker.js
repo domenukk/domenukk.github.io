510687f9bafe1c7c6cbe810f70c216a8a11cf526
Dominik Maier <dmnk@google.com>
2025-01-26 21:09:07 +0100

    Final export of BobaPop

--- bobapop/BobaPop.service.worker.js
+++ bobapop/BobaPop.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737921024|1780196694';
+const CACHE_VERSION = '1737922039|2794772380';
 /** @type {string} */
 const CACHE_PREFIX = 'BobaPop-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
